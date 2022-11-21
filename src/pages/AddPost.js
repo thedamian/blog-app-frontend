37b124e2eadd5a979f97d7a190e1293b9234ba95
jonathans199 <jso1919@gmail.com>
2022-11-21 14:02:17 -0500

    updated post form

--- src/pages/AddPost.js
+++ src/pages/AddPost.js
@@ -3,8 +3,20 @@ import { useState } from 'react'
 export default function AddPost() {
   const [form, setForm] = useState({})
 
-  const handleForm = (e) => {
-    setForm({...form, [e.target.name]: e.target.value})
+  const submitPost = e => {
+    e.preventDefault()
+
+    fetch('http://localhost:4040', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    })
+  }
+
+  const handleForm = e => {
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
 
   console.log(form)
@@ -12,15 +24,19 @@ export default function AddPost() {
   return (
     <div className='container'>
       <h1>Add Post</h1>
-      <form action=''>
+      <form action='submit'>
         <label htmlFor=''>Author</label>
         <input name='author' type='text' onChange={handleForm} />
-
+        <br />
         <label htmlFor=''>Text</label>
         <input name='text' type='text' onChange={handleForm} />
-
+        <br />
         <label htmlFor=''>Date</label>
         <input name='date' type='date' onChange={handleForm} />
+        <br />
+        <button type='submit' onClick={submitPost}>
+          Add new post
+        </button>
       </form>
     </div>
   )
